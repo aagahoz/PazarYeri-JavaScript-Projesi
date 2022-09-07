70d10fd39e112d133cb65401db204c15a1e2797d
aagahoz <ozdemiragah24@gmail.com>
2022-09-07 11:01:42 +0300

    Girilen Müşteri ID sine göre müşterinin yaptığı siparişler ve siparişlerin detaylarının ekrana basılması

--- app.js
+++ app.js
@@ -3,56 +3,38 @@ const productLib = require('./libraries/product.js');
 const shopLib = require('./libraries/shop.js');
 const orderLib = require('./libraries/order.js');
 
-let user1 = new customerLib.Customer("Agah Özdemir", "Tuzla", "ozdemiragah24@gmail.com", "1234567890");
 
-console.log(user1.getDetails());
+//  *** Yapay VeriTabanı *** //
+let customer1 = new customerLib.Customer("Agah 1", "Tuzla", "ozdemiragah24@gmail.com", "1234567890");
+let customer2 = new customerLib.Customer("Agah 2", "Tuzla", "ozdemiragah24@gmail.com", "1234567890");
+let customer3 = new customerLib.Customer("Agah 3", "Tuzla", "ozdemiragah24@gmail.com", "1234567890");
+let customer4 = new customerLib.Customer("Agah 4", "Tuzla", "ozdemiragah24@gmail.com", "1234567890");
+let customer5 = new customerLib.Customer("Agah 5", "Tuzla", "ozdemiragah24@gmail.com", "1234567890");
+const customers = [customer1, customer2, customer3, customer4, customer5];
 
+let product1 = new productLib.Product("Product 1", 12, "Product 1 Description");
+let product2 = new productLib.Product("Product 2", 15, "Product 2 Description");
+let product3 = new productLib.Product("Product 3", 17, "Product 3 Description");
+let product4 = new productLib.Product("Product 4", 17, "Product 4 Description");
+let product5 = new productLib.Product("Product 5", 19, "Product 5 Description");
+let product6 = new productLib.Product("Product 6", 14, "Product 6 Description");
+let product7 = new productLib.Product("Product 7", 13, "Product 7 Description");
+let product8 = new productLib.Product("Product 8", 11, "Product 8 Description");
+let product9 = new productLib.Product("Product 9", 15, "Product 9 Description");
+let product10 = new productLib.Product("Product 10", 23, "Product 10 Description");
+const products = [product1, product2, product3, product4, product5, product6, product7, product8, product9, product10];
 
+let order1 = new orderLib.Order("pending", 100, customer1.ID, 1, [product1.ID, product2.ID]);
+let order2 = new orderLib.Order("pending", 150, customer2.ID, 1, [product1.ID, product3.ID]);
+let order3 = new orderLib.Order("pending", 390, customer2.ID, 1, [product3.ID, product5.ID, product1.ID, product2.ID]);
+let order4 = new orderLib.Order("pending", 542, customer4.ID, 1, [product4.ID, product10.ID, product7.ID, product5.ID]);
+let order5 = new orderLib.Order("pending", 235, customer5.ID, 1, [product5.ID, product9.ID, product1.ID, product7.ID]);
+const orders = [order1, order2, order3, order4, order5];
 
+let shop1 = new shopLib.Shop("Shop 1", "Tuzla", "tuzlamodern@gmail.com", "1234567890", [order1.ID, order2.ID, order3.ID]);
+let shop2 = new shopLib.Shop("Shop 2", "Gebze", "gebze@gmail.com.", "1234567890", [order4.ID, order5.ID]);
+const shops = [shop1, shop2];
+////////////////////////////
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// lib.Customer("Agah Özdemir", "Tuzla", "ozdemiragah24@gmail.com", "1234567890");
-
-
-// const customer = new Customer("Agah Özdemir", "Tuzla", "ozdemiragah24@gmail.com", "1234567890");
-// const customer2 = new Customer("Abdullah Özdemir", "Gebze", "ozdemiragah24@gmail.com", "1234567890");
-
-// console.log(customer.getDetails());
-// console.log(customer2.getDetails());
-
-// const order = new Order("pending", 100, customer.ID, 1, []);
-// console.log(order.getDetails());
-
-// const product = new Product("Laptop", 1000, "Acer Laptop");
-// console.log(product.getDetails());
-
-// const shop = new Shop("Shop", "Tuzla", "shop@gmail.com", "0216 434 54 23", []);
+// *** Girilen Müşteri ID sine göre müşterinin yaptığı siparişler ve siparişlerin detaylarının ekrana basılması *** //
+customerLib.getCustomerOrdersDetails(2, orders, products);
--- libraries/customer.js
+++ libraries/customer.js
@@ -26,4 +26,27 @@ class Customer {
     }
 }
 
-module.exports = { Customer };
+function getCustomerOrdersDetails(customerID, orders, products)
+{
+    let customerOrders = orders.filter(order => order.customerID === customerID);
+
+    console.log("Orders:");
+    customerOrders.forEach(order => {
+        console.log("Order ID: " + order.ID);
+        console.log("Order Status: " + order.status);
+        console.log("Order Total: " + order.total);
+        console.log("Order Date: " + order.date);
+        console.log("Order Products ");
+        console.log("");
+        order.products.forEach(productID => {
+            let product = products.find(product => product.ID === productID);
+            console.log("Product ID: " + product.ID);
+            console.log("Product Name: " + product.name);
+            console.log("Product Price: " + product.price);
+            console.log("Product Description: " + product.description);
+            console.log();
+        });
+    });
+}
+
+module.exports = { Customer, getCustomerOrdersDetails };
